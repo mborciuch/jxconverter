06385eb09c4d2868de9f3988df34de3d91c3f16b
michalborciuch <michal123borciuch@gmail.com>
2020-05-29 15:52:44 +0200

    Moved Repository

---
+++ jxconverterweb/src/main/java/com/mbor/jxconverter/JxconverterApplication.java
@@ -0,0 +1,12 @@
+package com.mbor.jxconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxconverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JxconverterApplication.class, args);
+    }
+}
---
+++ jxconverterweb/src/test/java/com/mbor/jxconverter/JxconverterApplicationTests.java
@@ -0,0 +1,12 @@
+package com.mbor.jxconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JxconverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
