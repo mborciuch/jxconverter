eb1b6fc2c9403918e42c041097cf0a472595d70f
michalborciuch <michal123borciuch@gmail.com>
2020-06-03 12:47:47 +0200

    Add InputValue class

---
+++ jxconverterweb/src/main/java/com/mbor/jxconverter/model/InputValue.java
@@ -0,0 +1,14 @@
+package com.mbor.jxconverter.model;
+
+public class InputValue {
+
+    String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
