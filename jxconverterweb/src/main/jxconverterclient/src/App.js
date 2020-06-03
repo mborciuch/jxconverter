24ede990da2c35b79b0889157a0e5801262630ec
michalborciuch <michal123borciuch@gmail.com>
2020-06-03 09:55:59 +0200

    Base app component

--- jxconverterweb/src/main/jxconverterclient/src/App.js
+++ jxconverterweb/src/main/jxconverterclient/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className="header">
+                    <h1>JSON to XML & XML to JSON Converter</h1>
+                </div>
+                <div className="description">
+                    <p>Put your data below</p>
+                </div>
+                <div id="xml-to-json--converter" className="converters-container">
+                    <div className="converter">
+                        <h2>xml</h2>
+                        <textarea id="xml-input-field">
+
+                        </textarea>
+                    </div>
+                    <div id="json-to-xml-converter" className="converter">
+                        <h2>json</h2>
+                        <textarea id="json-input-field">
+
+                        </textarea>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
+
 
 export default App;
